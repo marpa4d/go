45a42bd118bc161ee93d3d7a066000bbaa38348a
Marpa <Marpa3d@yandex.ru>
2025-01-30 13:17:39 +0300

    Initial commit

---
+++ src/algobigo/go.mod
@@ -0,0 +1,1 @@
+module github.com/marpa4d/go
---
+++ src/algobigo/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"time"
+) //TIP S
+
+func main() {
+	t := time.Now()
+	fmt.Printf("%v", t)
+} // ee GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
+// Also, you can try interactive lessons for GoLand by selecting 'Help | Learn IDE Features' from the main menu.
